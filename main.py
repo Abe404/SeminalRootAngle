67285d6817cb58c09caa40cfcc680ed351c79289
Abraham Smith <abe@abesmith.co.uk>
2023-09-19 14:15:56 +0200

    adding progress bar, linted but not tested

--- main.py
+++ main.py
@@ -1,10 +1,108 @@
 import sys
 import os
-import numpy as np
-from seminal_root_angle.extract import extract_all_angles
-from PyQt6.QtCore import Qt, QDateTime
+from extract import get_angles_from_image
+from PyQt6.QtCore import Qt, QDateTime, pyqtSignal, QThread
 from PyQt6.QtWidgets import QApplication, QMainWindow, QGridLayout, QPushButton, QFileDialog, QWidget, QLabel, QMessageBox, QProgressBar
-from PyQt6.QtGui import QImage, QPixmap
+from progress_widget import BaseProgressWidget
+import time
+import datetime
+import humanize
+
+
+def ls(dir_path):
+    # Don't show hidden files
+    # These can happen due to issues like file system 
+    # synchonisation technology. RootPainter doesn't use them anywhere
+    fnames = os.listdir(dir_path)
+    fnames = [f for f in fnames if f[0] != '.']
+    return fnames
+
+
+class ProgressWatchThread(QThread):
+    """
+    Runs another thread.
+    """
+    progress_change = pyqtSignal(int, int)
+    done = pyqtSignal()
+
+    def __init__(self, root_seg_dir,
+            im_dataset_dir, seed_seg_dir,
+            max_seed_points_per_im, debug_image_dir,
+            output_csv_path, error_csv_path,
+            radius=300):
+        super().__init__()
+
+        self.root_seg_dir = root_seg_dir
+        self.im_dataset_dir = im_dataset_dir
+        self.seed_seg_dir = seed_seg_dir
+        self.max_seed_points_per_im = max_seed_points_per_im
+        self.debug_image_dir = debug_image_dir
+        self.output_csv_path = output_csv_path
+        self.error_csv_path = error_csv_path
+        self.radius = radius
+
+    def run(self):
+
+        seg_fnames = os.listdir(self.seed_seg_dir)
+        seg_fnames = [s for s in seg_fnames if '.png' in s]
+        start = time.time()
+        total_images = len(seg_fnames)
+        self.progress_bar.setMaximum(total_images)
+
+
+        csv_file = open(self.output_csv_path, 'w+')
+        error_file = open(self.error_csv_path, 'w+')
+        print("file_name,angle_degrees,seed_index,seed_x,seed_y,seed_pixels", file=csv_file)
+        print("file_name,error_message,seed_index,seed_x,seed_y,seed_pixels", file=error_file)
+
+        for fname in seg_fnames:
+            print(f"Extracting angles:{seg_fnames.index(fname) + 1}/{len(seg_fnames)}", fname)
+            try:
+                get_angles_from_image(self.root_seg_dir, self.im_dataset_dir,
+                                      self.seed_seg_dir,
+                                      self.max_seed_points_per_im,
+                                      fname, self.radius,
+                                      csv_file, error_file,
+                                      self.debug_image_dir)
+            except Exception as error:
+                print(fname, error)
+                print('file_name,{error},NA,NA,NA,NA', file=error_file)
+                raise error
+
+        time_str = humanize.naturaldelta(datetime.timedelta(seconds=time.time() - start))
+        print('Extracting angles for', len(seg_fnames), 'images took', time_str)
+
+        while True:
+            done_fnames = ls(self.output_dir)
+            count = len(done_fnames)
+            if count >= self.total_images:
+                self.done.emit()
+                break
+            else:
+                self.progress_change.emit(count, self.total_images)
+                time.sleep(0.2)
+
+
+class ProgressWidget(BaseProgressWidget):
+    def __init__(self):
+        super().__init__('Extracting Angles')
+        self.watch_thread = None
+
+    def run(self, root_seg_dir,
+            im_dataset_dir, seed_seg_dir,
+            max_seed_points_per_im, debug_image_dir,
+            output_csv_path, error_csv_path,
+            radius=300):
+
+        self.watch_thread = ProgressWatchThread(root_seg_dir,
+            im_dataset_dir, seed_seg_dir,
+            max_seed_points_per_im, debug_image_dir,
+            output_csv_path, error_csv_path, radius)
+
+        self.watch_thread.progress_change.connect(self.onCountChanged)
+        self.watch_thread.done.connect(self.done)
+        self.watch_thread.start()
+
 
 class SeminalRootAngleExtractor(QMainWindow):
     def __init__(self):
@@ -27,6 +125,7 @@ class SeminalRootAngleExtractor(QMainWindow):
         self.layout.addWidget(self.progress_bar, 6, 0, 1, 2)
         self.progress_value = 0
 
+        self.progress_widget = None
 
         self.root_seg_dir_label = QLabel("Root Segmentation Directory: None")
         self.seed_seg_dir_label = QLabel("Seed Segmentation Directory: None")
@@ -39,7 +138,8 @@ class SeminalRootAngleExtractor(QMainWindow):
         self.create_input_dir_widgets("Input Photo", self.select_input_photo_folder, self.input_photo_dir_label, 2)
 
         # Create a folder selection button and label for the output directory
-        self.create_output_dir_widgets("Output", self.select_output_folder, self.output_dir_label, 3)
+        self.create_output_dir_widgets("Output",
+            self.select_output_folder, self.output_dir_label, 3)
 
         # Create a submit button
         self.submit_button = QPushButton("Extract angles")
@@ -142,24 +242,18 @@ class SeminalRootAngleExtractor(QMainWindow):
 
     def extract_angles(self, output_folder):
         print('extract angles')
-        
-        extract_all_angles(root_seg_dir=self.root_seg_dir,
-                           im_dataset_dir=input_photo_dir,
-                           seed_seg_dir=seed_seg_dir,
-                           max_seed_points_per_im=2,
-                           debug_image_dir=os.path.join(output_folder, 'debug_images'),
-                           output_csv_path=os.path.join(output_folder, 'angles.csv')
-                           error_csv_path=os.path.join(output_folder, 'errors.csv'))
+        self.progress_widget = ProgressWidget()
+        self.progress_widget.run(root_seg_dir=self.root_seg_dir,
+                                 im_dataset_dir=self.input_photo_dir,
+                                 seed_seg_dir=self.seed_seg_dir,
+                                 max_seed_points_per_im=2,
+                                 debug_image_dir=os.path.join(output_folder, 'debug_images'),
+                                 output_csv_path=os.path.join(output_folder, 'angles.csv'),
+                                 error_csv_path=os.path.join(output_folder, 'errors.csv'))
 
-        #         self.progress_value = 0
-        # 
-        #         for i in range(100):
-        #             self.progress_value += 0.01  # Increase the progress by 1%
-        #             self.progress_bar.setValue(int(self.progress_value * 100))  # Set the progress bar value
-        #             if self.progress_value >= 1:
-        #                 self.progress_label.setText("Extraction complete")
-        #             import time
-        #             time.sleep(0.01)
+        self.progress_widget.show()
+        self.close()
+
 
 def main():
     app = QApplication(sys.argv)
